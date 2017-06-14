2583c3696f5238c2ff34f0ba6a99a32b227c5475
chaochao <chtang@microsoft.com>
2017-06-14 15:54:43 +0000

    load js config file

--- cc-js.el
+++ cc-js.el
@@ -1,56 +1,57 @@
 ;; beautify js/html/css
-;; (require 'web-beautify) ;; Not necessary if using ELPA package
-;; (eval-after-load 'js2-mode
-;;   '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
-;; ;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
-;; (eval-after-load 'js
-;;   '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
 
-;; (eval-after-load 'json-mode
-;;   '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+(require 'web-beautify) ;; Not necessary if using ELPA package
+(eval-after-load 'js2-mode
+  '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
+(eval-after-load 'js
+  '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
 
-;; (eval-after-load 'sgml-mode
-;;   '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+(eval-after-load 'json-mode
+  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
 
-;; (eval-after-load 'web-mode
-;;   '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
+(eval-after-load 'sgml-mode
+  '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
 
-;; (eval-after-load 'css-mode
-;;   '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+(eval-after-load 'web-mode
+  '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
 
-;; ;; web-beautify config
-;; (eval-after-load 'js2-mode
-;;   '(add-hook 'js2-mode-hook
-;;              (lambda ()
-;;                (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
-;; ;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
-;; (eval-after-load 'js
-;;   '(add-hook 'js-mode-hook
-;;              (lambda ()
-;;                (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+(eval-after-load 'css-mode
+  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
 
-;; (eval-after-load 'json-mode
-;;   '(add-hook 'json-mode-hook
-;;              (lambda ()
-;;                (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+;; web-beautify config
+(eval-after-load 'js2-mode
+  '(add-hook 'js2-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
+(eval-after-load 'js
+  '(add-hook 'js-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
 
-;; (eval-after-load 'sgml-mode
-;;   '(add-hook 'html-mode-hook
-;;              (lambda ()
-;;                (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+(eval-after-load 'json-mode
+  '(add-hook 'json-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
 
-;; (eval-after-load 'web-mode
-;;   '(add-hook 'web-mode-hook
-;;              (lambda ()
-;;                (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+(eval-after-load 'sgml-mode
+  '(add-hook 'html-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
 
-;; (eval-after-load 'css-mode
-;;   '(add-hook 'css-mode-hook
-;;              (lambda ()
-;;                (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
+(eval-after-load 'web-mode
+  '(add-hook 'web-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
 
-;; ;;js-doc
-;; (add-hook 'js2-mode-hook
-;;            #'(lambda ()
-;;                (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
-;;                (define-key js2-mode-map "@" 'js-doc-insert-tag)))
+(eval-after-load 'css-mode
+  '(add-hook 'css-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
+
+;;js-doc
+(add-hook 'js2-mode-hook
+           #'(lambda ()
+               (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
+               (define-key js2-mode-map "@" 'js-doc-insert-tag)))
